89f538d92cd92206222a3287e8023a1c70153c11
Jim Knape <Jimwinthropknape@gmail.com>
2020-07-23 22:44:05 -0700

    connects proxy to all components

--- index.js
+++ index.js
@@ -2,9 +2,9 @@ const httpProxy = require('http-proxy');
 const express = require('express');
 const morgan = require('morgan');
 const cors = require('cors');
-const mainHost = 'http://localhost:3000';
-const reviewHost = 'http://localhost:3004';
-const attractionHost = 'http://localhost:3003';
+const mainHost = `http://${process.env.mainURL}`;
+const reviewHost = `http://${process.env.reviewURL}`;
+const attractionHost = `http://${process.env.nearbyURL}`;
 const experienceHost = `http://${process.env.expURL}`;
 
 const app = express();
@@ -25,13 +25,13 @@ app.get('/favicon.ico/', (req, res) => {
   res.sendStatus(200);
 });
 
-// app.get('/:id/imageMain/bundle.js', (req, res) => {
-//   proxy.web(req, res, {target: mainHost});
-// });
+app.get('/:id/imageMain/bundle.js', (req, res) => {
+  proxy.web(req, res, {target: mainHost});
+});
 
-// app.get('/api/carousels/:id', (req, res) => {
-//   proxy.web(req, res, {target: mainHost});
-// });
+app.get('/api/carousels/:id', (req, res) => {
+  proxy.web(req, res, {target: mainHost});
+});
 
 app.get('/:id/exp/bundle.js', (req, res) => {
   proxy.web(req, res, {target: experienceHost});
@@ -44,26 +44,26 @@ app.get('/:id/fonts/*', (req, res) => {
   proxy.web(req, res, {target: experienceHost});
 });
 
-// app.get('/:id/bestNearby/bundle.js', (req, res) => {
-//   proxy.web(req, res, {target: attractionHost});
-// });
+app.get('/:id/bestNearby/bundle.js', (req, res) => {
+  proxy.web(req, res, {target: attractionHost});
+});
 
-// app.get('/:id/api/nearbyattractions', (req, res) => {
-//   proxy.web(req, res, {target: attractionHost});
-// });
+app.get('/:id/api/nearbyattractions', (req, res) => {
+  proxy.web(req, res, {target: attractionHost});
+});
 
-// app.get('/:id/reviewsModule/bundle.js', (req, res) => {
-//   proxy.web(req, res, {target: reviewHost});
-// });
-// app.get('/:id/api/reviews', (req, res) => {
-//   proxy.web(req, res, {target: reviewHost});
-// });
-// app.patch('/:id/api/reviews/:reviewId', (req, res) => {
-//   proxy.web(req, res, {target: reviewHost});
-// });
-// app.patch('/:id/api/reviews/:reviewId/:imageId', (req, res) => {
-//   proxy.web(req, res, {target: reviewHost});
-// });
+app.get('/:id/reviewsModule/bundle.js', (req, res) => {
+  proxy.web(req, res, {target: reviewHost});
+});
+app.get('/:id/api/reviews', (req, res) => {
+  proxy.web(req, res, {target: reviewHost});
+});
+app.patch('/:id/api/reviews/:reviewId', (req, res) => {
+  proxy.web(req, res, {target: reviewHost});
+});
+app.patch('/:id/api/reviews/:reviewId/:imageId', (req, res) => {
+  proxy.web(req, res, {target: reviewHost});
+});
 
 proxy.on('error', function (err, req, res) {
   console.log(res);
